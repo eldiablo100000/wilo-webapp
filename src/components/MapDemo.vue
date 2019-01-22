d2ce6b13c6e3232069c572d440c2845bb9d6d0cb
Natale Vadalà <nato.vada@gmail.com>
2019-01-22 21:54:25 +0100

    Added markers on point and polygon features

--- src/components/MapDemo.vue
+++ src/components/MapDemo.vue
@@ -16,11 +16,21 @@
       </div>
 
       <vl-map ref="map" v-if="showMap" data-projection="EPSG:4326" renderer="webgl">
-        <vl-view :projection="projection" :center.sync="center" :rotation.sync="rotation" :zoom.sync="zoom" />
-        <vl-layer-image id="xkcd" v-if="image">
+        <vl-view :center.sync="center" :rotation.sync="rotation" :zoom.sync="zoom" />
+        <!--<vl-layer-image id="xkcd" v-if="image">
           <vl-source-image-static :url="imgUrl" :size="imgSize" :extent="imgExtent" :projection="projection"
                                   :attributions="imgCopyright"></vl-source-image-static>
-        </vl-layer-image>
+        </vl-layer-image>-->
+        <vl-geoloc @update:position="geolocPosition = center = $event">
+          <template slot-scope="geoloc">
+            <vl-feature v-if="geoloc.position" id="position-feature">
+              <vl-geom-point :coordinates="geoloc.position"></vl-geom-point>
+              <vl-style-box>
+                <vl-style-icon src="static/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+              </vl-style-box>
+            </vl-feature>
+          </template>
+        </vl-geoloc>
         <vl-graticule :show-labels="true" v-if="graticule">
           <vl-style-stroke :line-dash="[5, 10]" color="green" slot="stroke"></vl-style-stroke>
           <vl-style-text slot="lon" text-baseline="bottom">
@@ -40,11 +50,18 @@
           <vl-geom-point :coordinates="[0, 0]" />
         </vl-feature>
 
-        <vl-layer-vector id="features">
+        <vl-layer-vector id="features" >
           <vl-source-vector :features.sync="features" />
         </vl-layer-vector>
-
-        <vl-layer-vector id="draw-pane" v-if="drawType != null">
+        <template v-for="(item, index) in features">
+            <vl-feature :key="index" >
+              <vl-geom-point :coordinates="item.geometry.coordinates" :z-index="3"></vl-geom-point>
+              <vl-style-box>
+                <vl-style-icon src="static/marker.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+              </vl-style-box>
+            </vl-feature>
+        </template>
+        <vl-layer-vector id="draw-pane" v-if="drawType != null" :z-index="0">
           <vl-source-vector :features.sync="drawnFeatures" ident="draw-target" />
         </vl-layer-vector>
         <vl-interaction-draw :type="drawType" source="draw-target" v-if="drawType != null" />
@@ -69,18 +86,18 @@
 
 <script>
 import * as eventCondition from 'ol/events/condition'
-import {createProj, addProj} from 'vuelayers/lib/ol-ext'
+// import {createProj, addProj} from 'vuelayers/lib/ol-ext'
 
 const features = [
-  {
-    type: 'Feature',
-    id: 'feature-1',
-    properties: {},
-    geometry: {
-      type: 'Point',
-      coordinates: [10, 10]
-    }
-  }
+  // {
+  //   type: 'Feature',
+  //   id: 'feature-1',
+  //   properties: {},
+  //   geometry: {
+  //     type: 'Point',
+  //     coordinates: [10, 10]
+  //   }
+  // }
 ]
 
 const computed = {
@@ -89,28 +106,38 @@ const computed = {
   }
 }
 
-const methods = {}
-let size = [11399, 7163]
-let extent = [0, 0, ...size]
-// create custom projection for image
-// NOTE: VueLayers.olExt available only in UMD build
-// in ES build it should be imported explicitly from 'vuelayers/lib/ol-ext'
-let customProj = createProj({
-  code: 'xkcd-image',
-  units: 'pixels',
-  extent
-})
-// add it to the list of known projections
-addProj(customProj)
+const methods = {
+  getCenter (arr) {
+    var x = arr.map(function (a) { return a[0] })
+    var y = arr.map(function (a) { return a[1] })
+    var minX = Math.min.apply(null, x)
+    var maxX = Math.max.apply(null, x)
+    var minY = Math.min.apply(null, y)
+    var maxY = Math.max.apply(null, y)
+    return [(minX + maxX) / 2, (minY + maxY) / 2]
+  }
+}
+// let size = [700000, 1300000]
+// var extent = [0, 0, 700000, 1300000]
+// // NOTE: VueLayers.olExt available only in UMD build
+// // in ES build it should be imported explicitly from 'vuelayers/lib/ol-ext'
+// let customProj = createProj({
+//   code: 'xkcd-image',
+//   units: 'pixels',
+//   extent
+// })
+// // add it to the list of known projections
+// addProj(customProj)
 export default {
   name: 'app',
   computed,
   methods,
   data () {
     return {
-      zoom: 2,
+      geolocPosition: undefined,
+      zoom: 5,
       // maxZoom: 8,
-      center: [size[0] / 2, size[1] / 2],
+      center: [0, 0],
       rotation: 0,
       features,
       selectedFeatures: [],
@@ -119,15 +146,40 @@ export default {
       showMap: true,
       drawType: undefined,
       drawnFeatures: [],
-      selectByHover: false,
-      projection: customProj.getCode(),
-      imgUrl: 'https://imgs.xkcd.com/comics/online_communities.png',
-      imgCopyright: '© <a href="http://xkcd.com/license.html">xkcd</a>',
-      imgSize: size,
-      imgExtent: extent
+      selectByHover: false
+      // projection: customProj.getCode(),
+      // imgUrl: 'https://imgs.xkcd.com/comics/online_communities.png',
+      // imgCopyright: '© <a href="http://xkcd.com/license.html">xkcd</a>',
+      // imgSize: size,
+      // imgExtent: extent
     }
   },
   created () {
+  },
+  watch: {
+    drawnFeatures: function (val) {
+      console.log(this.features)
+      var index = val.length - 1
+      var type = val[index].geometry.type
+      var coord = null
+      var marker =
+      {
+        type: 'Feature',
+        id: 'feature-' + type + '-' + index,
+        properties: {},
+        geometry: {
+          type: type
+        }
+      }
+      if (type === 'Polygon') {
+        coord = this.getCenter(val[index].geometry.coordinates[0])
+      } else if (type === 'Point') {
+        coord = val[index].geometry.coordinates
+      }
+      marker.geometry.coordinates = coord
+      this.features.push(marker)
+      // console.log(this.features)
+    }
   }
 }
 </script>
