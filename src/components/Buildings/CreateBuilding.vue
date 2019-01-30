8d1c4cfcaf6bfd63ee93faf21dc25752f13575a1
Natale Vadalà <nato.vada@gmail.com>
2019-01-30 12:06:12 +0100

    Input from form of add building to ol-geolocator; added checkaddress button; IT'S NECESSARY TO PRESS ENTER AFTER CLICKING CHECK ADDRESS

--- src/components/Buildings/CreateBuilding.vue
+++ src/components/Buildings/CreateBuilding.vue
@@ -1,69 +1,132 @@
 <template>
-  <b-row>
-    <b-col cols="12">
-      <h2>
-        Add Building
-        <b-link href="#/buildings">(Building List)</b-link>
-      </h2>
-      <b-form @submit="onSubmit">
-        <b-form-group id="fieldsetHorizontal"
-                  horizontal
-                  :label-cols="4"
-                  breakpoint="md"
-                  label="Enter Title">
-          <b-form-input id="title" :state="state" v-model.trim="building.title"></b-form-input>
-        </b-form-group>
-        <b-form-group id="fieldsetHorizontal"
-                  horizontal
-                  :label-cols="4"
-                  breakpoint="md"
-                  label="Enter Address">
-          <b-form-input id="address" :state="state" v-model.trim="building.address"></b-form-input>
-        </b-form-group>
-        <b-form-group id="fieldsetHorizontal"
-                  horizontal
-                  :label-cols="4"
-                  breakpoint="md"
-                  label="Enter City">
-          <b-form-input  id="city" :state="state" v-model.trim="building.city"></b-form-input>
-        </b-form-group>
-        <b-form-group id="fieldsetHorizontal"
-                  horizontal
-                  :label-cols="4"
-                  breakpoint="md"
-                  label="Enter Description">
-            <b-form-textarea id="description"
-                       v-model="building.description"
-                       placeholder="Enter something"
-                       :rows="2"
-                       :max-rows="6">{{building.description}}</b-form-textarea>
-        </b-form-group>
-        <b-button type="submit" variant="primary">Save</b-button>
-      </b-form>
-    </b-col>
-  </b-row>
+  <div>
+    <b-row>
+      <b-col cols="12">
+        <h2>
+          Add Building
+          <b-link href="#/buildings">(Building List)</b-link>
+        </h2>
+        <b-form @submit="onSubmit">
+          <b-form-group id="fieldsetHorizontal"
+                    horizontal
+                    :label-cols="4"
+                    breakpoint="md"
+                    label="Enter Title">
+            <b-form-input id="title" :state="state" v-model.trim="building.title"></b-form-input>
+          </b-form-group>
+          <b-form-group id="fieldsetHorizontal"
+                    horizontal
+                    :label-cols="4"
+                    breakpoint="md"
+                    label="Enter Address">
+            <b-form-input id="address" :state="state" v-model.trim="building.address"></b-form-input>
+          </b-form-group>
+          <b-form-group id="fieldsetHorizontal"
+                    horizontal
+                    :label-cols="4"
+                    breakpoint="md"
+                    label="Enter City">
+            <b-form-input  id="city" :state="state" v-model.trim="building.city"></b-form-input>
+          </b-form-group>
+          <b-form-group id="fieldsetHorizontal"
+                    horizontal
+                    :label-cols="4"
+                    breakpoint="md"
+                    label="Enter Description">
+              <b-form-textarea id="description"
+                         v-model="building.description"
+                         placeholder="Enter something"
+                         :rows="2"
+                         :max-rows="6">{{building.description}}</b-form-textarea>
+          </b-form-group>
+          <b-button type="submit" variant="primary">Save</b-button>
+          <b-button v-if="checked" @click="onCheck" variant="success">Check address</b-button>
+          <b-button v-if="!checked" @click="onCheck" variant="danger">Check address</b-button>
+        </b-form>
+      </b-col>
+    </b-row>
+    <MapComponent></MapComponent>
+  </div>
 </template>
 
 <script>
 
 import axios from 'axios'
+import MapComponent from '../MapComponent.vue'
 
 export default {
   name: 'CreateBuilding',
+  components: {
+    MapComponent
+  },
   data () {
     return {
-      building: {}
+      building: {
+        address: '',
+        city: ''
+      },
+      checked: false,
+      state: 'required'
     }
   },
   watch: {
     'building.address': function (val) {
       console.log(val)
+      document.getElementsByClassName('gcd-gl-control')[0].className += ' gcd-gl-expanded'
+      var string = val + ' ' + this.building.city
+      document.getElementById('gcd-input-query').value = ''
+      for (var i in string) {
+        document.getElementById('gcd-input-query').value += string[i]
+      }
+      // setTimeout(function () { document.getElementById('gcd-input-query').focus() }, 2000)
     },
     'building.city': function (val) {
       console.log(val)
+      document.getElementsByClassName('gcd-gl-control')[0].className += ' gcd-gl-expanded'
+      var string = this.building.address + ' ' + val
+      document.getElementById('gcd-input-query').value = ''
+      for (var i in string) {
+        document.getElementById('gcd-input-query').value += string[i]
+      }
     }
   },
   methods: {
+    sleep (ms) {
+      return new Promise(resolve => setTimeout(resolve, ms))
+    },
+    onCheck: async function (evt) {
+      evt.preventDefault()
+      // document.getElementsByClassName('gcd-gl-control')[0].className += ' gcd-gl-expanded'
+      // var string = this.building.address + ' ' + this.building.city
+      // for (var i in string) {
+      //   document.getElementById('gcd-input-query').value += string[i]
+      // }
+      document.getElementById('gcd-input-query').focus()
+      // await this.sleep(3000)
+      this.pressChar(document.getElementById('gcd-input-query'), 'a')
+      // var results = document.getElementsByClassName('gcd-gl-result')
+      // if (results.length > 1) {
+      //   this.checked = true
+      // } else { this.checked = false }
+    },
+    pressChar (el, keyCode) {
+      try {
+        var eventObj = document.createEventObject ? document.createEventObject() : document.createEvent('Events')
+
+        if (eventObj.initEvent) {
+          eventObj.initEvent('keypress', true, true)
+        }
+
+        eventObj.keyCode = keyCode
+        eventObj.which = keyCode
+
+        el.dispatchEvent ? el.dispatchEvent(eventObj) : el.fireEvent('onkeydown', eventObj)
+        console.log(eventObj)
+      } catch (e) {
+        console.log(e)
+        alert('Your browser does not support this example!')
+      }
+    },
     onSubmit (evt) {
       evt.preventDefault()
       axios.post(`http://localhost:3000/building`, this.building)
---
+++ src/components/MapComponent.vue
@@ -0,0 +1,102 @@
+<template>
+  <div id="app">
+    <div style="height: 100%">
+      <vl-map ref="map" v-if="showMap" data-projection="EPSG:4326" renderer="webgl">
+        <vl-view :center.sync="center" :rotation.sync="rotation" :zoom.sync="zoom"  />
+
+        <vl-layer-tile>
+          <vl-source-osm />
+        </vl-layer-tile>
+
+       <vl-feature id="marker">
+          <vl-geom-point :coordinates="[0, 0]" />
+            <vl-style-box>
+              <vl-style-icon src="static/logo.png" :scale="0.4" :anchor="[0.5, 1]"></vl-style-icon>
+            </vl-style-box>
+        </vl-feature>
+
+      </vl-map>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as eventCondition from 'ol/events/condition'
+import Geocoder from 'ol-geocoder'
+
+const features = [
+]
+
+const computed = {
+  selectCondition () {
+    return this.selectByHover ? eventCondition.pointerMove : eventCondition.singleClick
+  }
+}
+
+const methods = {
+}
+
+export default {
+  name: 'app',
+  computed,
+  methods,
+  data () {
+    return {
+      geocoder: undefined,
+      // maxResolution: 5,
+      zoom: 5,
+      // maxZoom: 8,
+      center: [0, 0],
+      rotation: 0,
+      features,
+      image: false,
+      showMap: true
+    }
+  },
+  mounted () {
+    // now ol.Map instance is ready and we can work with it directly
+    this.geocoder = new Geocoder('nominatim', {
+      provider: 'osm',
+      lang: 'it',
+      placeholder: 'Search for ...',
+      limit: 5,
+      debug: false,
+      autoComplete: true,
+      keepOpen: true
+    })
+    // this.$refs.map.$map.addControl(geocoder)
+    // console.log(this.$refs.map.$map.getControls())
+    this.$refs.map.$createPromise.then(() => {
+      this.$refs.map.$map.addControl(this.geocoder)
+      // console.log(this.$refs.map.$map.getControls())
+      this.geocoder.on('addresschosen', function (evt) {
+        // it's up to you
+        console.info(evt)
+        alert('hello')
+      })
+    })
+  },
+  watch: {
+    geocoder: function (val) {
+      console.log(val)
+    }
+  }
+}
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  @import "~ol/ol";
+
+  html, body, #app {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: Helvetica, Arial, sans-serif;
+    overflow: hidden;
+
+    * {
+      box-sizing: border-box;
+    }
+  }
+</style>
